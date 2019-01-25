d982c4e74a16bd6465f39c510f213e511a4fb256
qijianguo <1633972602@qq.com>
2019-01-25 18:49:53 +0800

    学习vue-基础知识

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>info</div>
+  <div>
+    info
+    <button type="button" @click="add()">添加</button>
+  </div>
 </template>
 
 <script>
-export default {}
+import store from '@/store/store.js';
+export default {
+  name: 'info',
+  store,
+  methods: {
+    add () {
+      console.log('test')
+      store.commit('increase')
+    }
+  }
+}
 </script>
 
 <style scoped>
